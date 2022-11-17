0d16f8d1ea5ec8301604fc2ff1b5cbd2881af573
Ankush Anand <ankushanand718@gmail.com>
2022-11-17 11:41:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+<script src="https://kit.fontawesome.com/368651d9a2.js" crossorigin="anonymous"></script>
+ 
+    /** This section is only needed once per page if manually copying **/
+    if (typeof MauticSDKLoaded == 'undefined') {
+      var MauticSDKLoaded = true;
+      var head = document.getElementsByTagName('head')[0];
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = '/media/js/mautic-form.js?v09f1db76';
+      script.onload = function () {
+        MauticSDK.onLoad();
+      };
+      head.appendChild(script);
+      var MauticDomain = '';
+      var MauticLang = {
+        'submittingMessage': "Please wait..."
+      }
+    } else if (typeof MauticSDK != 'undefined') {
+      MauticSDK.onLoad();
+    }
