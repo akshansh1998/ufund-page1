0eeed86946a018d928cc26b1b8306efcd7f745b5
Ankush Anand <ankushanand718@gmail.com>
2022-12-01 15:58:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@
  
     /** This section is only needed once per page if manually copying **/
     if (typeof MauticSDKLoaded == 'undefined') {
-      var MauticSDKLoaded = true;
-      var head = document.getElementsByTagName('head')[0];
-      var script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src = '/media/js/mautic-form.js?v09f1db76';
-      script.onload = function () {
+        var MauticSDKLoaded = true;
+        var head            = document.getElementsByTagName('head')[0];
+        var script          = document.createElement('script');
+        script.type         = 'text/javascript';
+        script.src          = 'https://mautic.ufund.online/media/js/mautic-form.js?v09f1db76';
+        script.onload       = function() {
+            MauticSDK.onLoad();
+        };
+        head.appendChild(script);
+        var MauticDomain = 'https://mautic.ufund.online';
+        var MauticLang   = {
+            'submittingMessage': "Please wait..."
+        }
+    }else if (typeof MauticSDK != 'undefined') {
         MauticSDK.onLoad();
-      };
-      head.appendChild(script);
-      var MauticDomain = '';
-      var MauticLang = {
-        'submittingMessage': "Please wait..."
-      }
-    } else if (typeof MauticSDK != 'undefined') {
-      MauticSDK.onLoad();
     }
